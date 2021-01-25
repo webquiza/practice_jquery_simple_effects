2dcea8fd6dfcffe3afc1dea1863130e9047fb292
Carlos Urquiza <webquiza@gmail.com>
2021-01-24 22:25:33 -0800

    update ids to use #initially instead

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-  $("p").click(function() {
-    $(".walrus-showing").toggle();
-    $(".walrus-hidden").toggle();
+  $(".clickable").click(function() {
+    $("#initially-showing").toggle();
+    $("#initially-hidden").toggle();
   });
 });
